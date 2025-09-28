4f22847bac6b3647a6d1dab7e9942dbf50b29909
Helios Cao <caxza1990@mgial.com>
2025-09-28 00:54:11 -0400

    fixed customalert closebutton position

--- src/components/CustomAlert/CustomAlert.jsx
+++ src/components/CustomAlert/CustomAlert.jsx
@@ -1,18 +1,18 @@
-import { CloseButton, Alert } from "@chakra-ui/react";
+import { CloseButton, Alert, Flex } from "@chakra-ui/react";
 
 const CustomAlert = ({ status, alertMessage, onClose }) => {
   return (
     <Alert.Root status={status} alignItems="center">
       <Alert.Indicator />
-      <Alert.Title>{alertMessage}</Alert.Title>
-      <CloseButton
-        position="absolute"
-        right="2"
-        bg="transparent"
-        border="none"
-        _focus={{ boxShadow: "none", outline: "none", border: "none" }}
-        onClick={onClose}
-      />
+      <Flex justify="space-between" align="center" w="100%">
+        <Alert.Title>{alertMessage}</Alert.Title>
+        <CloseButton
+          bg="transparent"
+          border="none"
+          _focus={{ boxShadow: "none", outline: "none", border: "none" }}
+          onClick={onClose}
+        />
+      </Flex>
     </Alert.Root>
   );
 };
